3a6984bca4ce710701f09f46c912b259e81892c3
bullfrog <jiujiuli@qq.com>
2020-11-23 00:08:32 +0800

    named route

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      initialRoute: "/",
       theme: ThemeData(
         primarySwatch: Colors.yellow,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // register route table
+      routes: {
+        "new_page": (context) => NewRoute(),
+        "tip_route": (context) => TipRoute(),
+        "/": (context) => MyHomePage(title: "home page"),
+      },
     );
   }
 }
@@ -55,10 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             FlatButton(
               onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) {
-                      return new NewRoute();
-                    }));
+                Navigator.pushNamed(context, "new_page");
               },
               child: Text("open new route"),
               textColor:Colors.yellow)
@@ -84,6 +87,65 @@ class NewRoute extends StatelessWidget {
       ),
       body: Center(
         child: Text("This is  new route"),
+      ),
+    );
+  }
+}
+
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,
+}) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示")
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                child: Text("返回"),
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        onPressed: () async {
+          print("！2@@#@@");
+          // 这里的 await 会把当前线程阻塞，有点类似于 java 的 Future
+          var result = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return TipRoute(
+                    text: "我是提示！！！！",
+                  );
+                }
+              ),
+          );
+          print("路由返回值：$result");
+          print("!!!!!asdasda");
+        },
+        child: Text("打开提示页"),
       ),
     );
   }
