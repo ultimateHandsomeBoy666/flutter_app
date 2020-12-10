f17c2e94f34809af613f57509f23dc31817eaa17
bullfrog <jiujiuli@qq.com>
2020-12-10 23:59:31 +0800

    finish 3.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/rendering.dart';
@@ -20,15 +21,23 @@ class MyApp extends StatelessWidget {
       routes: {
         "new_page": (context) => NewRoute(),
         "tip_route": (context) => TipRoute(text: "!SDAS",),
-        "/": (context) => MyHomePage(title: "home page"),
+        // "/": (context) => MyHomePage(title: "home page"),
+        // "/": (context) => MyHomePage(title: "Home"),
       },
-      // onGenerateRoute 路由生成钩子
+      // onGenerateRoute 路由生成钩子,对于没有在 routes 中定义的 route，会走此方法
       onGenerateRoute: (settings) {
         return MaterialPageRoute(builder: (context) {
           String routeName = settings.name;
-          return TipRoute(text: routeName);
+          // switch (routeName) {
+          //   case:
+          // }
+          return new TapboxA();
         });
       },
+      // home: new FindAncestorRoute(),
+      // home: new CounterWidget(),
+      // home: new CupertinoTestRoute(),
+      home: new ParentWidgetC()
     );
   }
 }
@@ -168,6 +177,319 @@ class RandomWordsWidget extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: new Text(wordPair.toString()),
+    );
+  }
+}
+
+// StatefulWidget example
+class CounterWidget extends StatefulWidget {
+  const CounterWidget ({
+    Key key,
+    this.initValue: 0
+});
+
+  final int initValue;
+
+  _CounterWidgetState createState() => new _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _counter;
+
+  @override
+  void initState() {
+    _counter = widget.initValue;
+    print("initState");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Scaffold(
+      body: Center(
+        child: FlatButton(
+          child: Text("$_counter"),
+          onPressed: () => setState(() => ++_counter),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant CounterWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("didUpdateWidget");
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    print("deactivate");
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose");
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    print("reassemble");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies");
+  }
+}
+
+// 获取父 widget 的 State 对象
+class FindAncestorRoute extends StatelessWidget {
+  static GlobalKey<ScaffoldState> _globalKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _globalKey,
+      appBar: AppBar(
+        title: Text("子树中获取对象"),
+      ),
+      body: Center(
+        child: Builder(builder: (context) {
+          return RaisedButton(
+              onPressed: () {
+                // 通过 context 获取父级 widget 的 state
+                // ScaffoldState _state = context.findAncestorStateOfType<ScaffoldState>();
+
+                // 通过 of 静态方法获取
+                // ScaffoldState _state = Scaffold.of(context);
+
+                // 通过 globalKey 获取
+                ScaffoldState _state = _globalKey.currentState;
+
+                // _state.showSnackBar(
+                //   SnackBar(content: Text("我是snack bar"))
+                // );
+                print("hasDrawer = ${_state.hasDrawer}");
+                _state.openDrawer();
+              },
+              child: Text("显示 snackbar"),
+          );
+        },),
+      ),
+    );
+  }
+}
+
+class CupertinoTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Center(
+          child: CupertinoButton(
+            color: CupertinoColors.activeGreen,
+            child: Text("press"),
+            onPressed: () {},
+          ),
+        ),
+      navigationBar: CupertinoNavigationBar(
+        middle: Text("Cupertino Demo"),
+      ),
+    );
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => new _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            _active ? "Active" : "Inactive",
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: _active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("parent build called");
+    return new Container(
+      // 所谓父组件管理状态，就是把子组件需要的参数，在父组件中构造然后传入
+      // 很好奇，这个 active 的 bool 值，是值传递还是引用传递？
+      // 个人觉得和 java 一样是值传递，但是传递的是对象的引用值拷贝，
+      // 因为 dart 也是万物皆对象
+      child: new TapboxB(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxB extends StatelessWidget {
+  TapboxB({Key key, this.active: false, @required this.onChanged})
+  : super(key: key) {
+    print("TapboxB created");
+  }
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("tapBox B build called");
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? "Active" : "Inactive",
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+class ParentWidgetC extends StatefulWidget {
+  @override
+  _ParentWidgetCState createState() => new _ParentWidgetCState();
+}
+
+class _ParentWidgetCState extends State<ParentWidgetC> {
+  bool _active = false;
+
+  void _handleTapboxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new TapboxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapboxC extends StatefulWidget {
+  TapboxC({Key key, this.active: false, @required this.onChanged})
+    : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapboxCState createState() => new _TapboxCState();
+}
+
+class _TapboxCState extends State<TapboxC> {
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("tapBoxC build called");
+    return new GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            widget.active ? "Active" : "Inactive",
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: widget.active ? Colors.lightGreen[700] : Colors.grey[600],
+          border: _highlight ? new Border.all(
+            color: Colors.teal[700],
+            width: 10.0) : null,
+        ),
+      )
     );
   }
 }
