50bc8dab20b70e3da9ba3fb8014af1caf20c71d0
bullfrog <jiujiuli@qq.com>
2021-01-17 02:13:46 +0800

    add infinite listview

--- lib/main.dart
+++ lib/main.dart
@@ -50,11 +50,75 @@ class MyApp extends StatelessWidget {
       // home: WrapRoute()
       // home: ContainerRoute()
       // home: SingleChildScrollViewTestRoute()
-      home: TestListViewRoute(),
+      // home: TestListViewRoute(),
+      home: InfiniteListView(),
     );
   }
 }
 
+class InfiniteListView extends StatefulWidget {
+  @override
+  _InfiniteListViewState createState() => _InfiniteListViewState();
+}
+
+class _InfiniteListViewState extends State<InfiniteListView> {
+  static const loadingTag = "##loading##";
+  var _words = <String>[loadingTag];
+
+  void _retrieveData() {
+    Future.delayed(Duration(seconds: 2)).then((value) {
+      setState(() {
+        _words.insertAll(_words.length - 1,
+          generateWordPairs().take(20).map((e) => e.asPascalCase).toList());
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _retrieveData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              if (_words[index] == loadingTag) {
+                if (_words.length - 1 < 100) {
+                  _retrieveData();
+                  return Container(
+                    padding: const EdgeInsets.all(16.0),
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: 24.0,
+                      height: 24.0,
+                      child: CircularProgressIndicator(strokeWidth: 2.0,),
+                    ),
+                  );
+                } else {
+                  return Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(16.0),
+                      child: Text("没有更多了", style: TextStyle(color: Colors.grey),)
+                  );
+                }
+              }
+              return ListTile(
+                  leading: FlutterLogo(),
+                  trailing: Icon(Icons.more_vert),
+                  subtitle: Text("This is second line----"),
+                  title: Text(_words[index])
+              );
+            },
+            separatorBuilder: (context, index) => Divider(height: .0,),
+            itemCount: _words.length),
+      ),
+    );
+  }
+}
 
 
 class TestListViewRoute extends StatelessWidget {
