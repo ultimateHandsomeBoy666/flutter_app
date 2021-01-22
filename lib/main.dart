bc21a14f937102915ddca2c6c7d7c779bad1a20f
bullfrog <jiujiuli@qq.com>
2021-01-22 18:12:24 +0800

    finish 6.6

--- lib/main.dart
+++ lib/main.dart
@@ -55,10 +55,70 @@ class MyApp extends StatelessWidget {
       // home: TestHeaderListViewRoute(),
       // home: TestGridView(),
       // home: InfiniteGridView(),
-      home: CustomScrollViewTestRoute(),
+      // home: CustomScrollViewTestRoute(),
+      home: ScrollControllerTestRoute(),
     );
   }
 }
+
+class ScrollControllerTestRoute extends StatefulWidget {
+  @override
+  _ScrollControllerTestRouteState createState() => _ScrollControllerTestRouteState();
+}
+
+class _ScrollControllerTestRouteState extends State<ScrollControllerTestRoute> {
+  ScrollController _controller = new ScrollController();
+  bool showToTopBtn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      print(_controller.offset);
+      if (_controller.offset < 1000 && showToTopBtn) {
+        setState(() {
+          showToTopBtn = false;
+        });
+      } else if (_controller.offset >= 1000 && showToTopBtn == false) {
+        setState(() {
+          showToTopBtn = true;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("scroll controller"),),
+      body: Scrollbar(
+        child: ListView.builder(
+          itemCount: 100,
+          itemExtent: 50.0,
+          controller: _controller,
+          itemBuilder: (context, index) {
+            return ListTile(title: Text("$index"),);
+          },
+        ),
+      ),
+      floatingActionButton: !showToTopBtn ? null : FloatingActionButton(
+        child: Icon(Icons.arrow_upward),
+        onPressed: () {
+          _controller.animateTo(.0,
+              duration: Duration(milliseconds: 200),
+              curve: Curves.bounceInOut);
+        },
+      ),
+    );
+  }
+}
+
 
 class CustomScrollViewTestRoute extends StatelessWidget {
   @override
