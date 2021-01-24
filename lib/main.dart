6acff3f61d05e08402ee30edff0233d1573f9fd9
bullfrog <jiujiuli@qq.com>
2021-01-24 15:08:49 +0800

    finsih6.6

--- lib/main.dart
+++ lib/main.dart
@@ -56,10 +56,58 @@ class MyApp extends StatelessWidget {
       // home: TestGridView(),
       // home: InfiniteGridView(),
       // home: CustomScrollViewTestRoute(),
-      home: ScrollControllerTestRoute(),
+      // home: ScrollControllerTestRoute(),
+      home: ScrollNotificationTestRoute(),
     );
   }
 }
+
+class ScrollNotificationTestRoute extends StatefulWidget {
+  @override
+  _ScrollNotificationTestRouteState createState() => _ScrollNotificationTestRouteState();
+}
+
+class _ScrollNotificationTestRouteState extends State<ScrollNotificationTestRoute> {
+
+  String _progress = "0%";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scrollbar(
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (notification) {
+            double progress = notification.metrics.pixels / notification.metrics.maxScrollExtent;
+            setState(() {
+              _progress = "${(progress * 100).toInt()}";
+            });
+            print("BottomEdge: ${notification.metrics.extentAfter == 0}");
+            // 返回 true 代表消耗掉这个通知
+            return true;
+          },
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              ListView.builder(
+                itemCount: 100,
+                itemExtent: 50.0,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text("#index"));
+                },
+              ),
+              CircleAvatar(
+                radius: 30.0,
+                child: Text(_progress),
+                backgroundColor: Colors.black54,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 class ScrollControllerTestRoute extends StatefulWidget {
   @override
