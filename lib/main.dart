e084ae7a1d9c97715d7393b087e329969bbdac10
bullfrog <jiujiuli@qq.com>
2021-01-26 13:36:20 +0800

    finish 7.5

--- lib/main.dart
+++ lib/main.dart
@@ -60,7 +60,64 @@ class MyApp extends StatelessWidget {
       // home: ScrollControllerTestRoute(),
       // home: ScrollNotificationTestRoute(),
       // home: WillPopScopeTestRoute(),
-      home: InheritedWidgetTestRoute(),
+      // home: InheritedWidgetTestRoute(),
+      home: ThemeTestRoute(),
+    );
+  }
+}
+
+class ThemeTestRoute extends StatefulWidget {
+  @override
+  _ThemeTestRouteState createState() => _ThemeTestRouteState();
+}
+
+class _ThemeTestRouteState extends State<ThemeTestRoute> {
+  Color _themeColor = Colors.teal;
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData themeData = Theme.of(context);
+    return Theme(
+      data: ThemeData(
+        primarySwatch: _themeColor,
+        iconTheme: IconThemeData(color: _themeColor)
+      ),
+      child: Scaffold(
+        appBar: AppBar(title: Text("主题测试")),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.favorite),
+                Icon(Icons.airport_shuttle),
+                Text("颜色跟随主题"),
+              ],
+            ),
+            Theme(
+              data: themeData.copyWith(
+                  iconTheme: themeData.iconTheme.copyWith(color: Colors.black)),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.favorite),
+                  Icon(Icons.airport_shuttle),
+                  Text("颜色固定黑色")
+                ],
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              _themeColor = _themeColor == Colors.teal ? Colors.blue : Colors.teal;
+            });
+          },
+          child: Icon(Icons.palette)
+        ),
+      ),
     );
   }
 }
