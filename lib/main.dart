59de6960c95ecfac8ed7e9217b62d2b254712a51
bullfrog <jiujiuli@qq.com>
2021-02-06 17:49:01 +0800

    finish 9.3

--- lib/main.dart
+++ lib/main.dart
@@ -82,6 +82,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class FadeRoute extends PageRoute {
+
+  FadeRoute({
+    @required this.builder,
+    this.transitionDuration = const Duration(milliseconds: 300),
+    this.opaque = true,
+    this.barrierDismissible = false,
+    this.barrierColor,
+    this.barrierLabel,
+    this.maintainState = true,
+});
+
+  final WidgetBuilder builder;
+
+  @override
+  final Duration transitionDuration;
+
+  @override
+  final bool opaque;
+
+  @override
+  final bool barrierDismissible;
+
+  @override
+  final Color barrierColor;
+
+  @override
+  final String barrierLabel;
+
+  @override
+  final bool maintainState;
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) {
+   return builder(context);
+  }
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child) {
+   return FadeTransition(
+     opacity: animation,
+     child: builder(context),
+   );
+  }
+}
+
 class Route1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
