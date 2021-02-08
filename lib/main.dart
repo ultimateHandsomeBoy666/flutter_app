c3a417bae6f07e1ea45984530f42a054eb4b4fe5
bullfrog <jiujiuli@qq.com>
2021-02-08 11:52:51 +0800

    add content

--- lib/main.dart
+++ lib/main.dart
@@ -138,6 +138,94 @@ class HeroAnimationRouteB extends StatelessWidget {
   }
 }
 
+class SlideTransitionX extends AnimatedWidget {
+
+  SlideTransitionX({
+    Key key,
+    @required Animation<double> position,
+    this.transformHitTests = true,
+    this.direction = AxisDirection.down,
+    this.child,
+}) : assert(position != null),
+  super(key: key, listenable:  position) {
+    // 偏移在内部处理
+    switch (direction) {
+      case AxisDirection.up:
+        _tween = Tween(begin: Offset(0, 1), end: Offset(0, 0));
+        break;
+      case AxisDirection.right:
+        _tween = Tween(begin: Offset(-1, 0), end: Offset(0, 0));
+        break;
+      case AxisDirection.down:
+        _tween = Tween(begin: Offset(0, -1), end: Offset(0, 0));
+        break;
+      case AxisDirection.left:
+        _tween = Tween(begin: Offset(1, 0), end: Offset(0, 0));
+        break;
+    }
+  }
+
+  Animation<double> get position => listenable;
+  final bool transformHitTests;
+  final Widget child;
+  final AxisDirection direction;
+  Tween<Offset> _tween;
+
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = _tween.evaluate(position);
+    if (position.status == AnimationStatus.reverse) {
+      switch (direction) {
+        case AxisDirection.up:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.right:
+          offset = Offset(-offset.dx, -offset.dy);
+          break;
+        case AxisDirection.down:
+          offset = Offset(offset.dx, -offset.dy);
+          break;
+        case AxisDirection.left:
+          offset = Offset(-offset.dx, offset.dy);
+          break;
+      }
+    }
+
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
+    );
+  }
+}
+
+class MySlideTransition extends AnimatedWidget {
+  MySlideTransition({
+    Key key,
+    @required Animation<Offset> position,
+    this.transformHitTests = true,
+    this.child,
+}) : assert(position != null),
+  super(key : key, listenable: position);
+  
+  Animation<Offset> get position => listenable;
+  final bool transformHitTests;
+  final Widget child;
+  
+  @override
+  Widget build(BuildContext context) {
+    Offset offset = position.value;
+    if (position.status == AnimationStatus.reverse) {
+      offset = Offset(-offset.dx, offset.dy);
+    }
+    return FractionalTranslation(
+      translation: offset,
+      transformHitTests: transformHitTests,
+      child: child,
+    );
+  }
+}
+
 class AnimatedSwitcherCounterRoute extends StatefulWidget {
   @override
   _AnimatedSwitcherCounterRouteState createState() => _AnimatedSwitcherCounterRouteState();
@@ -154,21 +242,28 @@ class _AnimatedSwitcherCounterRouteState extends State<AnimatedSwitcherCounterRo
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            AnimatedSwitcher(
-              duration: const Duration(milliseconds: 500),
-              transitionBuilder: (Widget child, Animation<double> animation) {
-                return ScaleTransition(
-                    child: child,
-                    scale: animation);
-              },
-              child: Text(
-                "$_count",
-                //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
-                key: ValueKey<int>(_count),
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headline4,
+            Center(
+              child: SizedBox(
+                width: 400.0,
+                height: 100.0,
+                child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 500),
+                    transitionBuilder: (Widget child, Animation<double> animation) {
+                      return SlideTransitionX(
+                          child: child,
+                          direction: AxisDirection.up,
+                          position: animation);
+                    },
+                  child: Text(
+                    "$_count",
+                    //显示指定key，不同的key会被认为是不同的Text，这样才能执行动画
+                    key: ValueKey<int>(_count),
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4,
+                  ),
+                ),
               ),
             ),
             RaisedButton(
