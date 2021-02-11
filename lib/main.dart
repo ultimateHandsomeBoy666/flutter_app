4ae57426df404f49bae132e2077533803dfa46a2
bullfrog <jiujiuli@qq.com>
2021-02-11 15:50:03 +0800

    finish 9.7

--- lib/main.dart
+++ lib/main.dart
@@ -82,10 +82,145 @@ class MyApp extends StatelessWidget {
       // home: HeroAnimationRoute(),
       // home: AnimatedSwitcherCounterRoute(),
       // home: AnimatedSwitcherCounterRoute(),
-      home: AnimatedDecoratedBoxRoute(),
+      // home: AnimatedDecoratedBoxRoute(),
+      home: AnimatedWidgetTestRoute(),
     );
   }
 }
+
+class AnimatedWidgetTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("AnimatedWidgetTestRoute"),),
+      body: AnimatedWidgetTest(),
+    );
+  }
+}
+
+
+class AnimatedWidgetTest extends StatefulWidget {
+  @override
+  _AnimatedWidgetTestState createState() => _AnimatedWidgetTestState();
+}
+
+class _AnimatedWidgetTestState extends State<AnimatedWidgetTest> {
+  double _padding = 10;
+  var _align = Alignment.topRight;
+  double _height = 100;
+  double _left = 0;
+  Color _color = Colors.red;
+  TextStyle _style = TextStyle(color: Colors.black);
+  Color _decorationColor = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    var duration = Duration(seconds: 5);
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          RaisedButton(
+            onPressed: () {
+              setState(() {
+                _padding = 20;
+              });
+            },
+            child: AnimatedPadding(
+              duration: duration,
+              padding: EdgeInsets.all(_padding),
+              child: Text("AnimatedPadding"),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Stack(
+              children: [
+                AnimatedPositioned(
+                  duration: duration,
+                  left: _left,
+                  child: RaisedButton(
+                    onPressed: () {
+                      setState(() {
+                        _left = 100;
+                      });
+                    },
+                    child: Text("AnimatedPositioned"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            color: Colors.grey,
+            child: AnimatedAlign(
+              duration: duration,
+              alignment: _align,
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    _align = Alignment.center;
+                  });
+                },
+                child: Text("AnimatedAlign"),
+              ),
+            ),
+          ),
+          AnimatedContainer(
+            duration: duration,
+            height: _height,
+            color: _color,
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  _height = 150;
+                  _color = Colors.blue;
+                });
+              },
+              child: Text("AnimatedContainer",
+                  style: TextStyle(color: Colors.white)),
+            ),
+          ),
+          AnimatedDefaultTextStyle(
+            child: GestureDetector(
+              child: Text("hello world"),
+              onTap: () {
+                setState(() {
+                  _style = TextStyle(
+                    color: Colors.blue,
+                    decorationStyle: TextDecorationStyle.solid,
+                    decorationColor: Colors.blue,
+                  );
+                });
+              },
+            ),
+            style: _style,
+            duration: duration,
+          ),
+          AnimatedDecoratedBox1(
+            duration: duration,
+            decoration: BoxDecoration(color: _decorationColor),
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  _decorationColor = Colors.red;
+                });
+              },
+              child: Text("AnimatedDecoratedBox",
+              style: TextStyle(color: Colors.white),),
+            ),
+          ),
+        ].map((e) {
+          return Padding(
+            padding: EdgeInsets.symmetric(vertical: 16),
+            child: e,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
 
 class AnimatedDecorBox extends ImplicitlyAnimatedWidget {
   AnimatedDecorBox({
