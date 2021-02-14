1391cc7221fe56f45b2d6ebc269916af3e788716
bullfrog <jiujiuli@qq.com>
2021-02-14 22:24:27 +0800

    finish 11.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:ffi';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
@@ -86,10 +87,71 @@ class MyApp extends StatelessWidget {
       // home: AnimatedSwitcherCounterRoute(),
       // home: AnimatedDecoratedBoxRoute(),
       // home: AnimatedWidgetTestRoute(),
-      home: FileOperationRoute(),
+      // home: FileOperationRoute(),
+      home: HttpTestRoute(),
     );
   }
 }
+
+class HttpTestRoute extends StatefulWidget {
+  @override
+  _HttpTestRouteState createState() => _HttpTestRouteState();
+}
+
+class _HttpTestRouteState extends State<HttpTestRoute> {
+  bool _loading = false;
+  String _text = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("HttpTestRoute"),),
+      body: ConstrainedBox(
+        constraints: BoxConstraints.expand(),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              RaisedButton(
+                child: Text("获取百度"),
+                onPressed: _loading ? null : () async {
+                  setState(() {
+                    _loading = true;
+                    _text = "正在请求。。。";
+                  });
+                  try {
+                    HttpClient httpclient = HttpClient();
+                    httpclient.idleTimeout = 50;
+                    HttpClientRequest request = await httpclient.getUrl(
+                        Uri.parse("https://www.baidu.com")
+                    );
+                    request.headers.add("user-agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
+                    HttpClientResponse response = await request.close();
+                    _text = await response.transform(utf8.decoder).join();
+                    print(response.headers);
+
+                    httpclient.close();
+
+                  } catch (e) {
+                    _text = "请求失败: $e";
+                  } finally {
+                    setState(() {
+                      _loading = false;
+                    });
+                  }
+                },
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width - 50.0,
+                child: Text(_text.replaceAll(RegExp(r"</title>") , "fuck")),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
 class FileOperationRoute extends StatefulWidget {
   FileOperationRoute({Key key}) : super(key: key);
