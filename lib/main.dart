638f98151c7251e07582ca7f6a4fc4d427963082
bullfrog <jiujiuli@qq.com>
2021-02-18 16:50:47 +0800

    finish 11.5

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/rendering.dart';
 import 'dart:core';
 import 'package:dio/dio.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/io.dart';
 
 void main() {
   runApp(MyApp());
@@ -89,10 +91,82 @@ class MyApp extends StatelessWidget {
       // home: FileOperationRoute(),
       // home: HttpTestRoute(),
       // home: FutureBuilderRoute1(),
-      home: ChunkDownloadRoute(),
+      // home: ChunkDownloadRoute(),
+      home: WebSocketRoute(),
     );
   }
 }
+
+class WebSocketRoute extends StatefulWidget {
+  @override
+  _WebSocketRouteState createState() => _WebSocketRouteState();
+}
+
+class _WebSocketRouteState extends State<WebSocketRoute> {
+  TextEditingController _controller = new TextEditingController();
+  IOWebSocketChannel channel;
+  String _text = "";
+
+  @override
+  void initState() {
+    channel = IOWebSocketChannel.connect('ws://echo.websocket.org');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("webScoket"),),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Form(
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(labelText: "Send a message"),
+              ),
+            ),
+            StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  _text = "connect error";
+                } else if (snapshot.hasData) {
+                  _text = "echo: " + snapshot.data;
+                }
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text(_text),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: "Send Message",
+        child: Icon(Icons.send),
+      ),
+    );
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    channel.sink.close();
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+
 
 class ChunkDownloadRoute extends StatefulWidget { 
   
